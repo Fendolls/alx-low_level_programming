42bb46f71e1bfd98228f32972af68bb7f6f59634
fendolls <mubarack6355@gmail.com>
2022-09-26 16:22:05 -0700

    third trial

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
-sh: 1: wq: not found
- * Desc: Header file containing prototypes for all functions
- * used in the 0x07-pointers_arrays_strings directory.
- */
-
 #include <stddef.h>
 
 int _putchar(char c);
@@ -20,3 +13,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif
